e08c0e21fd68fc6609a9d64abfec98495a04f788
Petchesi-Iulian <iamnotacat12@gmail.com>
2020-10-28 15:29:59 +0200

    Refs #12262: Added script to import taxonomies at install using collective.taxonomy (WIP)

--- eea/geolocation/setuphandlers.py
+++ eea/geolocation/setuphandlers.py
@@ -1,7 +1,25 @@
 """ Custom setup
 """
+import os
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
+from collective.taxonomy.factory import registerTaxonomy
+from collective.taxonomy.exportimport import TaxonomyImportExportAdapter, parseConfigFile
+
+
+try:
+    # Plone 4
+    from Products.CMFPlone.interfaces import IFactoryTool
+    IS_PLONE_4 = True
+except ImportError:
+    IS_PLONE_4 = False
+
+
+TAXONOMIES = {
+    'eea.geolocation.biotags.taxonomy': 'Biogeographical Regions',
+    'eea.geolocation.geotags.taxonomy': 'EEA Geolocation Geotags',
+    'eea.geolocation.countries_mapping.taxonomy': 'EEA Custom Country Names Mappings',
+}
 
 
 @implementer(INonInstallable)
@@ -20,10 +38,31 @@ class HiddenProfiles(object):
 def post_install(context):
     """ Post install script
     """
-    # Do something at the end of the installation of this package.
+    site = context.aq_parent
+    if IS_PLONE_4:
+        language = 'en'
+        directory = '/profiles/plone4imports/'
+    else:
+        language = 'en-us'
+        directory = '/profiles/plone5imports/'
+
+    for name, title in TAXONOMIES.items():
+        taxonomy = registerTaxonomy(site, name, title, language, 'Created at install')
+
+        path = os.path.dirname(os.path.realpath(__file__))
+        path += directory + name + '.xml'
+
+        # import pdb; pdb.set_trace()
+        with open(path) as file:
+            data = file.read().encode()
+            import_adapter = TaxonomyImportExportAdapter(site)
+            import_adapter.importDocument(taxonomy, data)
+
 
 
 def uninstall(context):
     """ Uninstall script
     """
     # Do something at the end of the uninstallation of this package.
+
+# 1.remove added taxonomies
